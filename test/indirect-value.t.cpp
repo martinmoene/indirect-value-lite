acdb49ec5ab7479e3dd5f54af31b4faf0441cf8d
Martin Moene <martin.moene@gmail.com>
2023-01-10 15:04:12 +0100

    Expand tests

--- test/indirect-value.t.cpp
+++ test/indirect-value.t.cpp
@@ -21,8 +21,7 @@ CASE( "indirect_value: Allows to default construct (empty)" )
 
 CASE( "indirect_value: Allows to construct from pointer" )
 {
-    int * p = new int( 7 );
-    indirect_value<int> iv( p );
+    const indirect_value<int> iv( new int( 7 ) );
 
     EXPECT(  iv      );
     EXPECT( *iv == 7 );
@@ -30,9 +29,8 @@ CASE( "indirect_value: Allows to construct from pointer" )
 
 CASE( "indirect_value: Allows to copy-construct" )
 {
-    int * p = new int( 7 );
     indirect_value<int> iv0;
-    indirect_value<int> iv1( p );
+    indirect_value<int> iv1( new int( 7 ) );
 
     indirect_value<int> iv2( iv0 );
     indirect_value<int> iv3( iv1 );
@@ -44,9 +42,8 @@ CASE( "indirect_value: Allows to copy-construct" )
 
 CASE( "indirect_value: Allows to move-construct" )
 {
-    int * p = new int( 7 );
-    indirect_value<int> iv2( indirect_value<int>{   } );
-    indirect_value<int> iv3( indirect_value<int>{ p } );
+    indirect_value<int> iv2( indirect_value<int>{              } );
+    indirect_value<int> iv3( indirect_value<int>{ new int( 7 ) } );
 
     EXPECT_NOT( iv2      );
     EXPECT(     iv3      );
@@ -55,9 +52,8 @@ CASE( "indirect_value: Allows to move-construct" )
 
 CASE( "indirect_value: Allows to copy-assign" )
 {
-    int * p = new int( 7 );
     indirect_value<int> iv0;
-    indirect_value<int> iv1( p );
+    indirect_value<int> iv1( new int( 7 ) );
     indirect_value<int> iv2;
 
     iv2 = iv1;
@@ -72,9 +68,8 @@ CASE( "indirect_value: Allows to copy-assign" )
 
 CASE( "indirect_value: Allows to move-assign" )
 {
-    int * p = new int( 7 );
     indirect_value<int> iv0;
-    indirect_value<int> iv1( p );
+    indirect_value<int> iv1( new int( 7 ) );
     indirect_value<int> iv2;
 
     iv2 = std::move( iv1 );
@@ -86,6 +81,320 @@ CASE( "indirect_value: Allows to move-assign" )
     iv2 = std::move( iv0 );
 
     EXPECT_NOT( iv2      );
+}
+
+CASE( "indirect_value: Allows to swap (value)" )
+{
+    indirect_value<int> iv3( new int( 3 ) );
+    indirect_value<int> iv7( new int( 7 ) );
+
+    iv3.swap( iv7 );
+
+    EXPECT( *iv3 == 7 );
+    EXPECT( *iv7 == 3 );
+}
+
+CASE( "indirect_value: Allows to swap (copier)" )
+{
+}
+
+CASE( "indirect_value: Allows to swap (deleter)" )
+{
+}
+
+CASE( "indirect_value: Allows to check if engaged, has_value()" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS
+    indirect_value<int> iv;
+    indirect_value<int> iv7( new int( 7 ) );
+
+    EXPECT_NOT(  iv.has_value() );
+    EXPECT(     iv7.has_value() );
+#else
+    EXPECT( !!"indirect_value: has_value() is not available (nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS=1)" );
+#endif
+}
+
+struct S { int x = 7; };
+
+CASE( "indirect_value: Allows to obtain value (op->)" )
+{
+    indirect_value<S> ivs7( new S );
+
+    EXPECT( ivs7->x == 7 );
+}
+
+CASE( "indirect_value: Allows to obtain value (op-> const)" )
+{
+    indirect_value<S> ivs7( new S );
+
+    EXPECT( ivs7->x == 7 );
+}
+
+CASE( "indirect_value: Allows to obtain value (op* &)" )
+{
+    indirect_value<int> iv7( new int( 7 ) );
+
+    EXPECT( *iv7 == 7 );
+}
+
+CASE( "indirect_value: Allows to obtain value (op* const &)" )
+{
+    const indirect_value<int> iv7( new int( 7 ) );
+
+    EXPECT( *iv7 == 7 );
+}
+
+CASE( "indirect_value: Allows to obtain value (op* &&)" )
+{
+    indirect_value<int> iv7( new int( 7 ) );
+
+    EXPECT( *std::move(iv7) == 7 );
+}
+
+CASE( "indirect_value: Allows to obtain value (op* const &&)" )
+{
+    const indirect_value<int> iv7( new int( 7 ) );
+
+    EXPECT( *std::move(iv7) == 7 );
+}
+
+CASE( "indirect_value: Allows to obtain value (value() &)" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS
+    indirect_value<int> iv7( new int( 7 ) );
+
+    EXPECT( iv7.value() == 7 );
+#else
+    EXPECT( !!"indirect_value: value() is not available (nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS=1)" );
+#endif
+}
+
+CASE( "indirect_value: Allows to obtain value (value() const &)" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS
+    const indirect_value<int> iv7( new int( 7 ) );
+
+    EXPECT( iv7.value() == 7 );
+#else
+    EXPECT( !!"indirect_value: value() is not available (nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS=1)" );
+#endif
+}
+
+CASE( "indirect_value: Allows to obtain value (value() &&)" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS
+    indirect_value<int> iv7( new int( 7 ) );
+
+    EXPECT( std::move(iv7).value() == 7 );
+#else
+    EXPECT( !!"indirect_value: value() is not available (nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS=1)" );
+#endif
+}
+
+CASE( "indirect_value: Allows to obtain value (value() const &&)" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS
+    const indirect_value<int> iv7( new int( 7 ) );
+
+    EXPECT( std::move(iv7).value() == 7 );
+#else
+    EXPECT( !!"indirect_value: value() is not available (nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS=1)" );
+#endif
+}
+
+CASE( "indirect_value: Throws on bad value access (value() &)" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS
+    indirect_value<int> iv;
+
+    EXPECT_THROWS_AS( iv.value(), bad_indirect_value_access );
+#else
+    EXPECT( !!"indirect_value: value() is not available (nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS=1)" );
+#endif
+}
+
+CASE( "indirect_value: Throws on bad value access (value() const &)" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS
+    const indirect_value<int> iv;
+
+    EXPECT_THROWS_AS( iv.value(), bad_indirect_value_access );
+#else
+    EXPECT( !!"indirect_value: value() is not available (nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS=1)" );
+#endif
+}
+
+CASE( "indirect_value: Throws on bad value access (value() &&)" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS
+    indirect_value<int> iv;
+
+    EXPECT_THROWS_AS( std::move(iv).value(), bad_indirect_value_access );
+#else
+    EXPECT( !!"indirect_value: value() is not available (nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS=1)" );
+#endif
+}
+
+CASE( "indirect_value: Throws on bad value access (value() const &&)" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS
+    const indirect_value<int> iv;
+
+    EXPECT_THROWS_AS( std::move(iv).value(), bad_indirect_value_access );
+#else
+    EXPECT( !!"indirect_value: value() is not available (nsiv_CONFIG_NO_EXTENSION_VALUE_MEMBERS=1)" );
+#endif
+}
+
+CASE( "indirect_value: Allows to obtain copier (get_copier() &)" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_GET_CPY_DEL_MEMBERS
+
+#else
+    EXPECT( !!"indirect_value: get_copier() is not available (nsiv_CONFIG_NO_EXTENSION_GET_CPY_DEL_MEMBERS=1)" );
+#endif
+}
+
+CASE( "indirect_value: Allows to obtain copier (get_copier() const &)" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_GET_CPY_DEL_MEMBERS
+
+#else
+    EXPECT( !!"indirect_value: get_copier() is not available (nsiv_CONFIG_NO_EXTENSION_GET_CPY_DEL_MEMBERS=1)" );
+#endif
+}
+
+CASE( "indirect_value: Allows to obtain deleter (get_deleter() &)" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_GET_CPY_DEL_MEMBERS
+
+#else
+    EXPECT( !!"indirect_value: get_deleter() is not available (nsiv_CONFIG_NO_EXTENSION_GET_CPY_DEL_MEMBERS=1)" );
+#endif
+}
+
+CASE( "indirect_value: Allows to obtain deleter (get_deleter() const &)" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_GET_CPY_DEL_MEMBERS
+
+#else
+    EXPECT( !!"indirect_value: get_deleter() is not available (nsiv_CONFIG_NO_EXTENSION_GET_CPY_DEL_MEMBERS=1)" );
+#endif
+}
+
+CASE( "make_indirect_value(): Allows to in-place construct an indirect value from parameters" )
+{
+}
+
+CASE( "allocate_indirect_value(): Allows to in-place construct an indirect value from parameters, with given allocator" )
+{
+}
+
+CASE( "swap(): Allows to swap" )
+{
+}
+
+CASE( "relational operators: Allows to compare indirect_value-s" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_RELATIONAL_OPERATORS
+
+    indirect_value<int> iv3( new int( 3 ) );
+    indirect_value<int> iv7( new int( 7 ) );
+    indirect_value<int> iv9( new int( 9 ) );
+
+    EXPECT( (iv7 == iv7) );
+    EXPECT( (iv7 == iv7) );
+
+    EXPECT( (iv7 != iv3) );
+    EXPECT( (iv3 != iv7) );
+
+    EXPECT( (iv7 <  iv9) );
+    EXPECT( (iv3 <  iv7) );
+
+    EXPECT( (iv7 <= iv9) );
+    EXPECT( (iv3 <= iv7) );
+
+    EXPECT( (iv7 >  iv3) );
+    EXPECT( (iv9 >  iv7) );
+
+    EXPECT( (iv7 >= iv3) );
+    EXPECT( (iv9 >= iv7) );
+#else
+    EXPECT( !!"relational operators: comparison is not available (nsiv_CONFIG_NO_EXTENSION_RELATIONAL_OPERATORS=1)" );
+#endif
+}
+
+// TODO: _enable_if_comparable_with_equal etc. do not (yet) allow for the likes of op==(indirect_value, nullptr_t).
+
+CASE( "relational operators: Allows to compare indirect_value with nullptr" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_RELATIONAL_OPERATORS
+    indirect_value<int> iv7( new int( 7 ) );
+
+    // EXPECT( (    iv7 == nullptr) );
+    // EXPECT( (nullptr == iv7) );
+
+    // EXPECT( (    iv7 != nullptr) );
+    // EXPECT( (nullptr != iv7) );
+
+    // EXPECT( (    iv7 < nullptr) );
+    // EXPECT( (nullptr < iv7) );
+
+    // EXPECT( (    iv7 <= nullptr) );
+    // EXPECT( (nullptr <= iv7) );
+
+    // EXPECT( (    iv7 >  nullptr) );
+    // EXPECT( (nullptr >  iv7) );
+
+    // EXPECT( (    iv7 >= nullptr) );
+    // EXPECT( (nullptr >= iv7) );
+
+    // EXPECT_NOT( (iv7 ==   nullptr) );
+    // EXPECT_NOT( (  nullptr == iv7) );
+#else
+    EXPECT( !!"relational operators: comparison is not available (nsiv_CONFIG_NO_EXTENSION_RELATIONAL_OPERATORS=1)" );
+#endif
+}
+
+CASE( "relational operators: Allows to compare indirect_value with value convertible to its value_type" )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_RELATIONAL_OPERATORS
+
+    indirect_value<int> iv7( new int( 7 ) );
+
+    EXPECT( (iv7 ==   7) );
+    EXPECT( (  7 == iv7) );
+
+    EXPECT( (iv7 !=   3) );
+    EXPECT( (  3 != iv7) );
+
+    EXPECT( (iv7 <    9) );
+    EXPECT( (  3 <  iv7) );
+
+    EXPECT( (iv7 <=   9) );
+    EXPECT( (  3 <= iv7) );
+
+    EXPECT( (iv7 >    3) );
+    EXPECT( (  9 >  iv7) );
+
+    EXPECT( (iv7 >=   3) );
+    EXPECT( (  9 >= iv7) );
+
+    EXPECT_NOT( (iv7 ==   3) );
+    EXPECT_NOT( (  3 == iv7) );
+#else
+    EXPECT( !!"relational operators: comparison is not available (nsiv_CONFIG_NO_EXTENSION_RELATIONAL_OPERATORS=1)" );
+#endif
+}
+
+CASE( "std::hash: Allows to ..." )
+{
+#if !nsiv_CONFIG_NO_EXTENSION_STD_HASH
+
+#else
+    EXPECT( !!"std::hash: std::hash specialisation is not available (nsiv_CONFIG_NO_EXTENSION_STD_HASH=1)" );
+#endif
 }
 
 // TODO: expand tests
